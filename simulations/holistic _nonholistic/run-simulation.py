ffc05a7eb5d201981df32ca5087ee38a5f9438a0
javiergonzalezh <j.h.gonzalez@sheffield.ac.uk>
2015-08-19 09:42:57 +0100

    results from simulation added

--- simulations/holistic _nonholistic/run-simulation.py
+++ simulations/holistic _nonholistic/run-simulation.py
@@ -16,20 +16,18 @@ from GPyOpt.fmodels.experimentsNd import *
 ## Simulation setup    
 experiments = [ 'func = cosines(sd=.1)',
                 'func = branin(sd=.1)',
-#                'func = goldstein(sd=.1)',
-#                'func = sixhumpcamel(sd=.1)',
-#                'func = mccormick(sd=.1)',
-#                'func = powers(sd=.1)',
+                'func = sixhumpcamel(sd=.1)',
+                'func = mccormick(sd=.1)',
+                'func = powers(sd=.1)',
 #                'func = alpine1(input_dim=2,sd=.1)',
 #                'func = alpine1(input_dim=5,sd=.1)',
 #                'func = alpine1(input_dim=10,sd=.1)'
               ]
 
-NR              = 5             # Different initial points.
+NR              = 10            # Different initial points.
 n_init          = 5             # number of initial points (per dimension).
 max_iter_dim    = 10            # Number of iterations (per dimension).
 
-experiment = experiments[0]
 for experiment in experiments:
     # --- problem setup
     exec experiment
@@ -38,9 +36,10 @@ for experiment in experiments:
     input_dim = len(bounds)
     
     # --- Matrices to save results
-    res_GLASSES_full  = np.empty([1,2])
-    res_GLASSES_O3    = np.empty([1,2])
-    res_GLASSES_O5    = np.empty([1,2])
+    res_GLASSES_02    = np.empty([1,2])
+    res_GLASSES_03    = np.empty([1,2])
+    res_GLASSES_05    = np.empty([1,2])
+    res_GLASSES_10    = np.empty([1,2])
     res_EL            = np.empty([1,2])
 
     # --- replicates 
@@ -50,35 +49,44 @@ for experiment in experiments:
         Y = f(X)
 
         # --- Crete the optimization objects
-        GLASSES_full = GPyOptmsa.msa.GLASSES(f,bounds, X,Y) 
-        GLASSES_O3   = GPyOptmsa.msa.GLASSES(f,bounds, X,Y) 
-        GLASSES_O5   = GPyOptmsa.msa.GLASSES(f,bounds, X,Y)
+        GLASSES_02 = GPyOptmsa.msa.GLASSES(f,bounds, X,Y) 
+        GLASSES_03   = GPyOptmsa.msa.GLASSES(f,bounds, X,Y) 
+        GLASSES_05   = GPyOptmsa.msa.GLASSES(f,bounds, X,Y)
+        GLASSES_10   = GPyOptmsa.msa.GLASSES(f,bounds, X,Y)
         EL           = GPyOpt.methods.BayesianOptimization(f=f,bounds=bounds, X=X, Y=Y ,acquisition='EL1') 
 
         # --- Run the optimizations
         print experiment
         print k
-        print 'res_GLASSES_full'
-        GLASSES_full.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=2)
-        rep_col     = [k]*GLASSES_full.Y_best.shape[0]
-        res_GLASSES_full = np.vstack((res_GLASSES_full,np.vstack((rep_col,GLASSES_full.Y_best)).T))
-        np.savetxt('res_GLASSES_full'+experiment[7:]+'.txt', res_GLASSES_full)
+        print 'res_GLASSES_02'
+        GLASSES_02.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=2)
+        rep_col     = [k]*GLASSES_02.Y_best.shape[0]
+        res_GLASSES_02 = np.vstack((res_GLASSES_02,np.vstack((rep_col,GLASSES_02.Y_best)).T))
+        np.savetxt('res_GLASSES_02'+experiment[7:]+'.txt', res_GLASSES_02)
 
         print experiment
         print k
-        print 'GLASSES_O3'
-        GLASSES_O3.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=3) 
-        rep_col     = [k]*GLASSES_O3.Y_best.shape[0]
-        res_GLASSES_O3    = np.vstack((res_GLASSES_O3,np.vstack((rep_col,GLASSES_O3.Y_best)).T))
-        np.savetxt('res_GLASSES_O3_'+experiment[7:]+'.txt', res_GLASSES_O3)
+        print 'GLASSES_03'
+        GLASSES_03.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=3) 
+        rep_col     = [k]*GLASSES_03.Y_best.shape[0]
+        res_GLASSES_03    = np.vstack((res_GLASSES_03,np.vstack((rep_col,GLASSES_03.Y_best)).T))
+        np.savetxt('res_GLASSES_03_'+experiment[7:]+'.txt', res_GLASSES_03)
     
         print experiment
         print k
-        print 'GLASSES_O5'
-        GLASSES_O5.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=5)  
-        rep_col     = [k]*GLASSES_O5.Y_best.shape[0]
-        res_GLASSES_O5    = np.vstack((res_GLASSES_O5,np.vstack((rep_col,GLASSES_O5.Y_best)).T))
-        np.savetxt('res_GLASSES_O5_'+experiment[7:]+'.txt', res_GLASSES_O5)
+        print 'GLASSES_05'
+        GLASSES_05.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=5)  
+        rep_col     = [k]*GLASSES_05.Y_best.shape[0]
+        res_GLASSES_05    = np.vstack((res_GLASSES_05,np.vstack((rep_col,GLASSES_05.Y_best)).T))
+        np.savetxt('res_GLASSES_05_'+experiment[7:]+'.txt', res_GLASSES_05)
+
+        print experiment
+        print k
+        print 'GLASSES_10'
+        GLASSES_10.run_optimization(max_iter=max_iter_dim*input_dim,n_ahead=10)  
+        rep_col     = [k]*GLASSES_10.Y_best.shape[0]
+        res_GLASSES_10    = np.vstack((res_GLASSES_10,np.vstack((rep_col,GLASSES_10.Y_best)).T))
+        np.savetxt('res_GLASSES_10_'+experiment[7:]+'.txt', res_GLASSES_10)
 
         print experiment
         print k
@@ -91,25 +99,43 @@ for experiment in experiments:
     
 # -------- make comparison plot 
 # choose experiment
-experiment = experiments[0] 
-n_inits      = 26 
-n_replicates = 4
 
-# -------- load results
-best_res_GLASSES_full   = np.loadtxt('res_GLASSES_full_'+experiment[7:]+'.txt')[1:(n_inits*n_replicates+1),1].reshape(n_replicates,n_inits).mean(0)
-best_GLASSES_O3       = np.loadtxt('res_GLASSES_O3_'+experiment[7:]+'.txt')[1:(n_inits*n_replicates+1),1].reshape(n_replicates,n_inits).mean(0)
-best_GLASSES_O5       = np.loadtxt('res_GLASSES_O5_'+experiment[7:]+'.txt')[1:(n_inits*n_replicates+1),1].reshape(n_replicates,n_inits).mean(0)
-best_HBO10      = np.loadtxt('res_HBO10_'+experiment[7:]+'.txt')[1:(n_inits*n_replicates+1),1].reshape(n_replicates,n_inits).mean(0)
-best_EL       = np.loadtxt('res_EL_'+experiment[7:]+'.txt')[1:(n_inits*n_replicates+1),1].reshape(n_replicates,n_inits).mean(0)
+results_mean = np.zeros((5,5))
+results_min = np.zeros((5,5))
+results_median = np.zeros((5,5))
 
-plt.plot(best_HBOfull,label='full GLASSES')
-plt.plot(best_GLASSES_O3 ,label='GLASSES-3-steps')
-plt.plot(best_GLASSES_O5,label='GLASSES-5-steps')
-plt.plot(best_EL,'b-.',label='myopic-EL')
-plt.xlabel('iteration')
-plt.ylabel('Best value')
 
-plt.legend()
+for k in range(len(experiments)): 
+    results_mean[k,:] = create_table(experiments[k]).mean(1)
+    results_min[k,:] = create_table(experiments[k]).min(1)
+    results_median[k,:] = np.median(create_table(experiments[k]),1)
+
+
+
+methods = ['EL','GL-2','GL-3','GL-5','GL-10']
+
+
+def create_table(experiment):
+    
+    results = np.zeros((5,10))
+    results[0,:] = value_at_last(np.loadtxt('res_EL_'+experiment[7:]+'.txt')[1:,:])
+    results[1,:] = value_at_last(np.loadtxt('res_GLASSES_02_'+experiment[7:]+'.txt')[1:,:])
+    results[2,:] = value_at_last(np.loadtxt('res_GLASSES_03_'+experiment[7:]+'.txt')[1:,:])
+    results[3,:] = value_at_last(np.loadtxt('res_GLASSES_05_'+experiment[7:]+'.txt')[1:,:])
+    results[4,:] = value_at_last(np.loadtxt('res_GLASSES_10_'+experiment[7:]+'.txt')[1:,:])
+    return results
+
+
+def value_at_last(data):
+    data_val = data[:,1]
+    data_id  = data[:,0]
+    from itertools import groupby
+    return np.array([data_val[data_id==i].min() for i, _ in groupby(data_id)])
+
+
+
+
+
 
 
 
